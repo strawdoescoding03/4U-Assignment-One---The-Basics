e9056af0ca5c88c21bc7f66aacc6e322421e3529
362575441 <362575441@2260I-L29-019>
2025-09-04 09:36:25 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+
+namespace _4U_Assignment_One___The_Basics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName, favMovie;
+
+            firstName = "Sarah";
+            favMovie = "The Matrix";
+            
+            Console.WriteLine($"Hello, my name is {firstName.ToLower()} y mi película favorita es {favMovie.ToLower()}.");
+            firstName = firstName.ToUpper();
+            favMovie = favMovie.ToUpper();
+
+           Console.WriteLine($"Hello, my name is {firstName} y mi película favorita es {favMovie}.");
+           
+
+
+
+        }
+    }
+}
