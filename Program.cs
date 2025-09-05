4fff9ae1d1f566b31714ddbfb2400223356c2432
362575441 <362575441@2260I-L29-019>
2025-09-05 09:07:42 -0400

    e

--- Program.cs
+++ Program.cs
@@ -6,19 +6,48 @@ namespace _4U_Assignment_One___The_Basics
     {
         static void Main(string[] args)
         {
-            string firstName, favMovie;
+            // Variables
+            string firstName, favMovie, favQuote;
 
             firstName = "Sarah";
             favMovie = "The Matrix";
-            
-            Console.WriteLine($"Hello, my name is {firstName.ToLower()} y mi película favorita es {favMovie.ToLower()}.");
+            favQuote = "There is no spoon.";
+
+            //1
+            Console.WriteLine($"Hello, je m'appelle {firstName.ToLower()} y mi película favorita es {favMovie.ToLower()}.");
             firstName = firstName.ToUpper();
             favMovie = favMovie.ToUpper();
 
-           Console.WriteLine($"Hello, my name is {firstName} y mi película favorita es {favMovie}.");
-           
+            Console.WriteLine($"Hello, je m'appelle {firstName} y mi película favorita es {favMovie}.");
+            Console.WriteLine(favMovie.Contains("THE"));
+            favMovie = favMovie.Replace("A", "@");
+            favMovie = favMovie.Replace("E", "3");
 
-           
+            Console.WriteLine(favMovie);
+
+            Console.WriteLine("Press Enter to Move...");
+            Console.ReadLine();
+            Console.Clear();
+
+
+            //2
+            favQuote = favQuote.ToLower();
+            favQuote = favQuote.Replace("a", " ");
+            favQuote = favQuote.Replace("e", " ");
+            favQuote = favQuote.Replace("i", " ");
+            favQuote = favQuote.Replace("o", " ");
+            favQuote = favQuote.Replace("u", " ");
+
+            Console.WriteLine(favQuote);
+
+
+            favQuote = "There is no spoon.";
+
+            favQuote = favQuote.Replace("no", "yes");
+            //favQuote = (favQuote.Trim(new char[] { "a", "e", "i", "o", "u" }));
+
+
+
 
         }
     }
