b89d6d13148c950a7c1756b9ecea4ff4f92c047a
362575441 <362575441@2260I-L29-019>
2025-09-08 09:09:51 -0400

    Finished

--- Program.cs
+++ Program.cs
@@ -32,11 +32,29 @@ namespace _4U_Assignment_One___The_Basics
 
             //2
             favQuote = favQuote.ToLower();
-            favQuote = favQuote.Replace("a", "").Replace("e", " ").Replace("i", " ").Replace("o", " ").Replace("u", " ");;
-
-
+            favQuote = favQuote.Replace("a", "").Replace("e", "").Replace("i", "").Replace("o", "").Replace("u", "");
 
             Console.WriteLine(favQuote);
+
+            //3
+
+            
+            Console.Write("       __             __ _,-7'~^7* -.");     Console.WriteLine("                  //            __..--''``---....___   _..._    __");
+            Console.Write("     _// )      _,-L;'~`         `.");       Console.WriteLine("                    /// //_.-'    .-/l';  `        ``<._  ``.''_ `. / // /");
+            Console.Write("   .l ( /`L' -,-L`                 ;");      Console.WriteLine("                   ///_.-' _..--.'_    ll                   `( ) ) // //");
+            Console.Write("  / 6;");                                    Console.WriteLine("                                                 /// (_..-' // (< _     ;_..__               ; `' / ///"); 
+            Console.Write(" /           ,             ,-l''     ;");    Console.WriteLine("                 /// // // //  `-._,_)' // / ``--...____..-' /// / //");
+            Console.WriteLine("(, __.--.      ll           /        ;");    ///cat
+            Console.WriteLine("            //'   /`-.ll   |          | `._________");
+            Console.WriteLine("            _.- '_/`  )  )--...,,,___ll ll-----------,)");
+            Console.WriteLine("          (((l''~` _.-'.-'           __`-.   )");        
+            Console.WriteLine("            (((l''`             (((---~l''`");       
+            Console.WriteLine();
+            //rat
+            // ASCII Art from https://emojicombos.com/ under "ASCII Art" section
+
+
+
 
 
 
